8e6489047ef1f761a722212ba576cfa6b39f0857
HelloZXB <Hello16369@163.com>
2021-02-14 19:34:15 +0800

    feat: 插件配置

--- notes/.vuepress/config/headConf.js
+++ notes/.vuepress/config/headConf.js
@@ -16,5 +16,12 @@ module.exports = [
   ['meta', { // 设置关键字
     name: 'keywords',
     content: '朱晓冰, 朱晓冰的笔记, 文档, Vue Press'
-  }]
+  }],
+  ['meta', {name: 'theme-color', content: '#3eaf7c'}],
+  ['meta', {name: 'apple-mobile-web-app-capable', content: 'yes'}],
+  ['meta', {name: 'apple-mobile-web-app-status-bar-style', content: 'black'}],
+  ['link', {rel: 'apple-touch-icons', href: 'https://hellozxb.coding.net/p/notes/files/all/22990139/preview/22990333'}],
+  ['link', {rel: 'mask-icons', href: 'https://hellozxb.coding.net/p/notes/files/all/22990139/preview/22990332', color: '#3eaf7c'}],
+  ['meta', {name: 'msapplication-TileImage', content: 'https://hellozxb.coding.net/p/notes/files/all/22990139/preview/22990330'}],
+  ['meta', {name: 'msapplication-TileColor', content: '#000000'}],
 ]
